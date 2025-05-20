f71a24d91e9940b3e22981c38cca1ae8967f9eb7
caren580 <chumacaren69@gmail.com>
2025-05-20 18:44:56 +0300

    converting minutes to seconds

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -1,8 +1,14 @@
 //challenge 1 sum of 2 numbers
-function addition(num1, num2) {
+/*function addition(num1, num2) {
       
     return num1+num2
 }
 
 console.log(addition(8 , 5)) ;
+*/
+//challenge 2 converting minutes to seconds
 
+function convert(minutes){
+     return 60*minutes
+}
+console.log(convert(5));
