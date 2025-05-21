8b876b3b36e7dab5402b0ba2f114796a62a7a8a2
caren580 <chumacaren69@gmail.com>
2025-05-21 23:03:10 +0300

    find max number

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -1,10 +1,24 @@
+//challenge 1 sum of positives
 function sumOfPositives(arr){
-    let sum=0;
-    for(let i=0; i<arr.length; i++){
+    let sum = 0;
+    for(let i=0; i < arr.length; i++){
         if(arr[i] > 0){
             sum += arr[i];
         }
     }
     return sum;
 }
-console.log(sumOfPositives([1,-3,5,-2,9,-8]));
+//console.log(sumOfPositives(1,-3,5,-2,9,-8));//15
+
+
+//challenge 2 find maximum value
+function findMax(arr){
+    let max = arr[0];
+    for(let i = 1; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    return max;
+}
+console.log(findMax([3,7,2,9,5]));
